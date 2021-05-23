78bbfd4e0bbd3ca330763539f89b3872c427e293
svalenzuela1 <59404624+svalenzuela1@users.noreply.github.com>
2021-05-22 23:07:27 -0400

    function added

--- app.js
+++ app.js
@@ -1,13 +1,57 @@
 "use script"
 window.onload = function(){
-    let computeBtn = document.getElementById("compute")
-   computeBtn.onclick = test; 
+    let totalCostBtn = document.getElementById("totalCostBtn")
+    totalCostBtn.onclick = totalCostBtnClicked; 
 
 }
 
-function test(){
-let pickupDateField = document.getElementById("pickupDate");
+function totalCostBtnClicked(){
+let numberOfDaysField = document.getElementById("numberOfDays");
 
-console.log(pickupDateField.value)
+let numberOfDays = Number(numberOfDaysField.value);
 
+let carRental = 29.99 * numberOfDays;
+
+let options = 0;
+
+console.log(numberOfDaysField)
+console.log(numberOfDays, carRental, options)
+
+const electronicCheckbox = document.getElementById("electronicTollTag");
+    if (electronicCheckbox.checked == true) {
+        options += (3.95 * numberOfDays);
+    }
+
+const gpsCheckbox = document.getElementById("GPS");
+    if (gpsCheckbox.checked == true) {
+        options += (2.95 * numberofDays);
+    }
+    
+const roadsideCheckbox = document.getElementById("roadsideAssistance");
+    if (roadsideCheckbox.checked == true) {
+        options += (2.95 * numberofDays);
+    }
+
+    let surcharge = 0;
+
+    const radioYesBtn = document.getElementById("radioYes")
+    if (radioYesBtn.checked == true) {
+        surcharge = carRental * .30;
+    }
+
+    let totalCost = carRental + options + surcharge;
+
+    console.log(numberOfDays, carRental, options, totalCost)
+
+    const carRentalPara = document.getElementById("carRental");
+    carRentalPara.innerHTML = "Car rental: " + carRental.toFixed(2);
+
+    const optionsPara = document.getElementById("options");
+    optionsPara.innerHTML = "Options " + options.toFixed(2);
+
+    const surchargePara = document.getElementById("surcharge");
+    surchargePara.innerHTML = "Under 25 Surcharge: " + surcharge.toFixed(2);
+ 
+    const totalCostPara = document.getElementById("totalCost");
+    totalCostPara.innerHTML = "Total due: " + totalCost.toFixed(2);
 }
